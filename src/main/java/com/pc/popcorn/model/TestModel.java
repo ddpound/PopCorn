e1eb1fe4fd7eb6ba01a497a6d7a24e147d0abe42
dpound <dpound@naver.com>
2021-08-29 20:57:45 +0900

    Repostory 와 model 패키지 추가

---
+++ src/main/java/com/pc/popcorn/model/TestModel.java
@@ -0,0 +1,9 @@
+package com.pc.popcorn.model;
+
+import javax.persistence.Entity;
+
+
+
+// @Entity
+public class TestModel {
+}
---
+++ src/main/java/com/pc/popcorn/repository/TestRepository.java
@@ -0,0 +1,6 @@
+package com.pc.popcorn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository{
+}
